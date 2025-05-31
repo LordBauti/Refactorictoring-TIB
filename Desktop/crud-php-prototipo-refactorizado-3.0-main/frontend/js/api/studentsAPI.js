b8e2dbb5fff53619bf0ea356e56613b6f1bac01c
Bautista Hansen <bauto.chimango@gmail.com>
2025-05-31 18:16:11 -0300

    Delete Desktop/crud-php-prototipo-refactorizado-3.0-main directory
    Borre el anterior crud dado que al hacer el merge no se elimino.

--- Desktop/crud-php-prototipo-refactorizado-3.0-main/frontend/js/api/studentsAPI.js
+++
@@ -1,3 +0,0 @@
-/*frontend/js/api/studentsAPI.js*/
-import { createAPI } from './apiFactory.js';
-export const studentsAPI = createAPI('students');
--- Desktop/crud-php-prototipo-refactorizado-3.0-main/frontend/js/api/subjectsAPI.js
+++
@@ -1,3 +0,0 @@
-/*frontend/js/api/subjectsAPI.js*/
-import { createAPI } from './apiFactory.js';
-export const subjectsAPI = createAPI('subjects');
